db5f9f6eb992e5dfbf135cf5e1ae37c36b858704
Joy Adah <joya6470@gmail.com>
2022-07-09 14:12:47 +0100

    first commit

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,8 @@
+<x-guest-layout>
+    <div class="container w-full px-5 py-6 mx-auto italic flex justify-center font-mono font-semibold animate-bounce">
+        <div>
+            <h1>Thank you!</h1>
+            <p>Your Reservation is ready!</p>
+        </div>
+    </div>
+</x-guest-layout>
